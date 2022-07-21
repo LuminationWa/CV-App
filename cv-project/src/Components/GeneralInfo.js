6580d8da2bb34917e84f1576e91fb895d259fd31
Edel <edelmedina322@gmail.com>
2022-07-21 19:04:57 -0300

    Added change handlers to GI inputs

--- cv-project/src/Components/GeneralInfo.js
+++ cv-project/src/Components/GeneralInfo.js
@@ -1,29 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import uniqid from "uniqid";
 
 class GeneralInfo extends Component {
-    constructor() {
-        super()
-    }
+  constructor() {
+    super();
+    this.state = {
+      firstName: { text: "", id: uniqid() },
+      lastName: { text: "" },
+      email: { text: "" },
+      phone: { text: "" },
+    };
+  }
 
-    render() {
-        return(
-            <div>
-                <h1>Personal information</h1>
-                <form>
-                    <label htmlFor='first-name'>First Name</label>
-                    <input type='text' id='first-name'></input>
-                    <label htmlFor='last-name'>Last Name</label>
-                    <input type='text' id='last-name'></input>
-                    <label htmlFor='email'>Email</label>
-                    <input type='text' id='email'></input>
-                    <label htmlFor='phone-number'>Phone Number</label>
-                    <input type='text' id='phone-number'></input>
-                    <button type='submit'>Add</button>
-                    <button type=''>Delete</button>
-                </form>
-            </div>
-        )
-    }
+  handleChange = (element, targetState) => {
+    this.setState({
+      [targetState]: {
+        text: element.target.value,
+      },
+    });
+  };
+
+  render() {
+    const { firstName, lastName, email, phone } = this.state;
+
+    return (
+      <div>
+        <h1>Personal information</h1>
+        <form>
+          <label htmlFor="first-name">First Name</label>
+          <input
+            type="text"
+            id="first-name"
+            onChange={(e) => this.handleChange(e, "firstName")}
+          />
+          <label htmlFor="last-name">Last Name</label>
+          <input
+            type="text"
+            id="last-name"
+            onChange={(e) => this.handleChange(e, "lastName")}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            onChange={(e) => this.handleChange(e, "email")}
+          />
+          <label htmlFor="phone-number">Phone Number</label>
+          <input
+            type="text"
+            id="phone-number"
+            onChange={(e) => this.handleChange(e, "phone")}
+          />
+          <button type="submit">Add</button>
+          <button type="">Delete</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default GeneralInfo;
